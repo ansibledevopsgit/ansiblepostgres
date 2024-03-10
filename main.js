8ed106658a5e86b75d3480c888bbec30b12c10b5
@mohamad66 <rahimi@homacall.com>
2024-03-10 13:25:13 +0330

    postgressssss

--- main.js
+++ main.js
@@ -9,23 +9,23 @@ const {  ILog  } = require("./ILog");
 const bodyParser = require('body-parser');
  
 const app = express();
-//app.use(cors());
+app.use(cors());
 
-var whitelist = ['http://109.122.224.141', 'http://109.122.224.141:3000','http://localhost','http://localhost:3000']
-var corsOptionsDelegate = function (req, callback) {
-  var corsOptions;
-  if (whitelist.indexOf(req.header('Origin')) !== -1) {
-    corsOptions = { origin: true } // reflect (enable) the requested origin in the CORS response
-  } else {
-    corsOptions = { origin: false } // disable CORS for this request
-  }
-  callback(null, corsOptions) // callback expects two parameters: error and options
-}
+// var whitelist = ['http://109.122.224.141', 'http://109.122.224.141:3000','http://192.168.1.103:3000','http://localhost:3000']
+// var corsOptionsDelegate = function (req, callback) {
+//   var corsOptions;
+//   if (whitelist.indexOf(req.header('Origin')) !== -1) {
+//     corsOptions = { origin: true } // reflect (enable) the requested origin in the CORS response
+//   } else {
+//     corsOptions = { origin: false } // disable CORS for this request
+//   }
+//   callback(null, corsOptions) // callback expects two parameters: error and options
+// }
 
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
-const hostname = 'localhost';
+//const hostname = 'localhost';
 const port = 9000;
 
 const pool =   new Pool({ 
@@ -38,17 +38,17 @@ const pool =   new Pool({
 
 logservice.Connection(pool)
 
-app.get("/",cors(corsOptionsDelegate),(req,res)=>{
- res.send("welcome my app PostgreSQL");
+app.get("/",(req,res)=>{
+ res.send("welcome my app PostgreSQL ...");
 });
 
-app.get("/Create",cors(corsOptionsDelegate),(req,res)=>{
+app.get("/Create",(req,res)=>{
     commentservice.Connection();
     res.send("Create  PostgreSQL");
 });
 
 
-app.get("/Delete",cors(corsOptionsDelegate),async(req,res)=>{
+app.get("/Delete",async(req,res)=>{
        let LogID = req.query.LogID;
        await logservice.Delete(LogID).then((state) =>{
         if(state){
@@ -59,7 +59,7 @@ app.get("/Delete",cors(corsOptionsDelegate),async(req,res)=>{
         }).catch((e)=>{ res.send( "Error : " + e )});
  });
 
- app.get("/GetByID", cors(corsOptionsDelegate),async (req,res)=>{
+ app.get("/GetByID", async (req,res)=>{
     let LogID = req.query.LogID;
    await  logservice.GetByID(LogID).then((Log) =>{
         if(Log != null){
@@ -71,7 +71,7 @@ app.get("/Delete",cors(corsOptionsDelegate),async(req,res)=>{
  
 });
 
-app.get("/GetAll", cors(corsOptionsDelegate),async (req,res)=>{
+app.get("/GetAll", async (req,res)=>{
 
     await  logservice.GetAll().then((Logs) =>{
         if(Logs != null){
@@ -87,7 +87,7 @@ app.get("/GetAll", cors(corsOptionsDelegate),async (req,res)=>{
   
 });
 
-app.get("/Insert",cors(corsOptionsDelegate),async(req,res)=>{
+app.get("/Insert",async(req,res)=>{
 
     const date  = new Date();  
     const Log = new ILog();
@@ -104,7 +104,7 @@ app.get("/Insert",cors(corsOptionsDelegate),async(req,res)=>{
 });
 
 
-app.get("/Update",cors(corsOptionsDelegate), async(req,res)=>{
+app.get("/Update", async(req,res)=>{
     let LogID = req.query.LogID;
     const date  = new Date();  
     const Log = new ILog();
@@ -122,8 +122,8 @@ app.get("/Update",cors(corsOptionsDelegate), async(req,res)=>{
 });
 
 
-app.listen(port, hostname, () => {
-    console.log(`Server running at http://${hostname}:${port}/`);
+app.listen(port, () => {
+    console.log(`Server running at http://:${port}/`);
   });
 
   
